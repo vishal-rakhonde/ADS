548ee99b119152e6f2b73b076e674881efd0ebd6
Vishal Rakhonde <161593302+vishal-rakhonde@users.noreply.github.com>
2024-06-25 23:19:28 +0530

    Update bstToGst.java

--- bstToGst.java
+++ bstToGst.java
@@ -26,3 +26,31 @@ The number of nodes in the tree is in the range [1, 100].
 0 <= Node.val <= 100
 All the values in the tree are unique */
 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int sum=0;
+    public TreeNode bstToGst(TreeNode root) {
+        if(root != null){
+            bstToGst(root.right);
+            sum= sum+root.val;
+            root.val =sum;
+            bstToGst(root.left);
+        }
+        return root;
+    }
+}
+
