00c396169d57f38cc4484be146ab851a302a34e9
Vishal Rakhonde <161593302+vishal-rakhonde@users.noreply.github.com>
2024-07-16 23:57:36 +0530

    Update balanceBST.java

--- balanceBST.java
+++ balanceBST.java
@@ -12,7 +12,6 @@ Explanation: This is not the only correct answer, [3,1,4,null,2] is also correct
 Example 2:
 Input: root = [2,1,3]
 Output: [2,1,3]
- 
 
 Constraints:
 The number of nodes in the tree is in the range [1, 104].
